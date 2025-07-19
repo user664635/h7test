84435c9a46a54173bc2617994ab7403685940296
user664635 <myy12358@gmail.com>
2025-07-19 19:21:47 +0800

    blink

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,20 @@
 
 typedef uint32_t u32;
 typedef uint8_t u8;
-#define gpioa (*(volatile u32 *)(0x58020014))
-extern "C" void reset() {
 
-  gpioa = 0xffffffff;
+#define sfr(a) ((volatile u32 *)(a))
+#define gpioa sfr(0x58020400)
+#define rcc sfr(0x58024400)
+#define syst sfr(0xe000e010)
+extern "C" void reset() {
+  rcc[0x38] = -1;
+  gpioa[0] &= 0x55555555;
+
+  syst[1] = -1;
+  syst[0] = 5;
+
   while (1) {
+    gpioa[5] = syst[2];
   }
 }
 
